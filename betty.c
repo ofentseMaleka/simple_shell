dbeaa52bdb2db676545e984886801f535198653a
ofentsemaleka6@gmail.com <ofentsemaleka6@gmail.com>
2023-11-01 04:58:20 -0700

    team

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ *
+ * Description: This is the main function that prints a greeting.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("hello Ade\n");
+	return (0);
+}
