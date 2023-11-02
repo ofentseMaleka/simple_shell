ec983140bcc0a9d93ed2a74ae92fb14f56c9d128
ofentsemaleka6@gmail.com <ofentsemaleka6@gmail.com>
2023-11-02 05:33:01 -0700

    team

---
+++ main_demo.c
@@ -0,0 +1,20 @@
+#ifndef _SIMPLE_H
+#define _SIMPLE_H
+
+#include <stdio.h> // for standard input /output //
+#include <unistd.h> //used for system call: fork, exec, pipe etc //
+#include <string.h> // working with strings//
+#include <stdlib.h> //standard library function//
+#include <sys/types.h> // data types used in system call//
+#include <errorno.h> //error code and related function//
+#include <perror.h> // for displaying error messages//
+#include <malloc.h> //memory allocation//
+#include <sys/wait.h> // for process control and waiting for child processes//
+#include <limits.h> //limits related to integral data types//
+#include <fcntl.h> //for file control function and oparations//
+#include <sys/stat.h> // used for file and directory handling//
+
+/**
+ * To convert numbers
+ */
+#endif 
