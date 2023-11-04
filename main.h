adac269c54fb2d4a193c0fbac6c504313e2a3590
ofentsemaleka6@gmail.com <ofentsemaleka6@gmail.com>
2023-11-04 11:10:37 -0700

    team

--- main.h
+++ main.h
@@ -1,18 +1,18 @@
 #ifndef _SIMPLE_H
 #define _SIMPLE_H
 
-#include <stdio.h> // for standard input /output //
-#include <unistd.h> //used for system call: fork, exec, pipe etc //
-#include <string.h> // working with strings//
-#include <stdlib.h> //standard library function//
-#include <sys/types.h> // data types used in system call//
-#include <errorno.h> //error code and related function//
-#include <perror.h> // for displaying error messages//
-#include <malloc.h> //memory allocation//
-#include <sys/wait.h> // for process control and waiting for child processes//
-#include <limits.h> //limits related to integral data types//
-#include <fcntl.h> //for file control function and oparations//
-#include <sys/stat.h> // used for file and directory handling//
+#include <stdio.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <sys/types.h>
+#include <errorno.h> 
+#include <perror.h>
+#include <malloc.h> 
+#include <sys/wait.h>
+#include <limits.h> 
+#include <fcntl.h>
+#include <sys/stat.h>
 
 void exec_com(const char *comand);
 void my_printf(char *message);
