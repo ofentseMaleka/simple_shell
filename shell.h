2b941e25f3d2544744c49a1fa4a3b75eb078ac21
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-09 11:31:25 -0800

    team

--- shell.h
+++ shell.h
@@ -17,7 +17,6 @@
 
 /* list of the prototype */
 
-const char *execute_command(const char *command)
 void exec_com(const char *command);
 void my_print(char *output);
 void prompt(void);
@@ -28,7 +27,7 @@ void display_prompt(void);
 void execute_function(const char *function, char **argv);
 char **parse_args(const char *input); /* Forward declaration */
 
-/* Change name of  function not to cause avoid conflicts with standard library functions */
+/* Change name of  function to avoid conflicts with standard library functions */
 int my_exit(char **args);
 int my_cd(char **args);
 int my_help(char **args);
